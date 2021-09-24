29e12fdc3675aa2ed419631f9b95de5ee9688be9
Artsiom Umbryian <trueweightlifter@gmail.com>
2021-09-24 19:58:31 +0300

    add Timeout and dynamic fonts

--- 1-slides/app.js
+++ 1-slides/app.js
@@ -1,14 +1,44 @@
-const slides = document.querySelectorAll('.slide');
+const slidePlagin = (i = 0) => {
+    const slides = document.querySelectorAll('.slide');
+    const clearActiveClasses = () => {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        });
+    }
+    slides[i].classList.add('active');
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'ArrowRight') {
+    clearActiveClasses();
+  	++i;
+    if (i >= slides.length) {
+            i = 0;
+        }
+  } else if (e.key === 'ArrowLeft') {
+  	clearActiveClasses();
+  	--i;
+    if (i < 0) {
+            i = slides.length - 1;
+        }
+  }
+  slides[i].classList.add('active');
+});
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        });
+    }
+
+setTimeout(function run() {
         clearActiveClasses();
-        slide.classList.add('active');
-    });
+        slides[i++].classList.add('active');
+        if (i === slides.length) {
+            i = 0;
+        }
+    setTimeout(run, 3000);
+    }, 0);
+
 }
 
-const clearActiveClasses = () => {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidePlagin(0);
